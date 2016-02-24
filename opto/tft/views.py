4df01678b62a0ff7f94a5ead2e1801aeb952e9fa
kerafyN <kerafyn1up@gmail.com>
2016-02-24 16:56:42 +0100

    24.02.2016
    --

--- opto/tft/views.py
+++ opto/tft/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
-    return render(request,"templates/tft/tft_displays_landing_en.html")
+    return render(request, "templates/tft/tft_displays_landing_en.html")
