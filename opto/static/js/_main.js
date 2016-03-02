b293b5d2f5c0d5ec63ba979d7330a37b35562d34
kerafyN <kerafyn1up@gmail.com>
2016-03-02 16:27:49 +0100

    slideshow owl + hidden nav on top
    working on opacity change of the nav, gonna change the <nav> to <div>

--- opto/static/js/_main.js
+++ opto/static/js/_main.js
@@ -1,24 +1,3 @@
-// ------ owl js ------
-$(document).ready(function() {
-
-  $("#owl-example").owlCarousel({
-
-      navigation : true, // Show next and prev buttons
-      slideSpeed : 300,
-      paginationSpeed : 400,
-      //singleItem:true
-
-      // "singleItem:true" is a shortcut for:
-       items : 1,
-       itemsDesktop : false,
-       itemsDesktopSmall : false,
-       itemsTablet: false,
-       itemsMobile : false,
-       autoplay:true,
-       loop:true,
-       margin: 10,
-       nav:true,
-       navigationText: false,
-    });
-
+$(document).on('scroll', function (e) {
+    $('.uk-navbar').css('opacity', ($(document).scrollTop() / 200));
 });
