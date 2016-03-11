b1d703e59643ed06411f1c17cd65ab63ce4e6e5c
kerafyN <kerafyn1up@gmail.com>
2016-03-11 15:52:49 +0100

    navbar show on scroll ok

--- opto/static/js/_main.js
+++ opto/static/js/_main.js
@@ -1,3 +1,26 @@
-$(document).on('scroll', function (e) {
-    $('.uk-navbar').css('opacity', ($(document).scrollTop() / 200));
+$(window).scroll(function() {
+// 100 = The point you would like to fade the nav in.
+
+	if ($(window).scrollTop() > 100 ){
+
+ 		$('.bg').addClass('show uk-animation-scale-up');
+
+  } else {
+
+    $('.bg').removeClass('show');
+
+ 	};
 });
+
+$('.scroll').on('click', function(e){
+		e.preventDefault()
+
+  $('html, body').animate({
+      scrollTop : $(this.hash).offset().top
+    }, 1500);
+});
+
+$('.bg').hover(
+       function(){ $(this).addClass('show') },
+       function(){ $(this).removeClass('uk-fadeOut show ') }
+)
